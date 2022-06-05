d252fb2f6c689c58a00a7b7c53b66b41227da434
toproad <tattun0817@gmail.com>
2022-06-06 06:20:24 +0900

    内容確認

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -75,7 +75,7 @@ validateRequiredInput = (...args) => {
               + '【問い合わせ内容】\n' + description
       };
 
-      const url = 'https://hooks.slack.com/services/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';
+      // const url = 'https://hooks.slack.com/services/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';
      
       // fetchメソッドでフォームの内容をSlackのIncoming Webhook URL に送信する
       fetch(url, {
